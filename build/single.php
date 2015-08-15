f78dac7a3b140348fc025bdbd90a27230d2dd5c3
sunya <mugen.Xyz@gmail.com>
2015-08-15 23:45:40 +0900

    SNS共有に対応した。

--- build/single.php
+++ build/single.php
@@ -10,8 +10,30 @@
         <footer class="article__footer">
           <?php the_tags('<ul class="article__footer__tags"><li>','</li><li>','</li></ul>');?>
           <ul class="article__footer__share">
-            <li class="share__twitter"><a href="#">Twitter</a></li>
-            <li class="share__facebook"><a href="#">Facebook</a></li>
+            <li class="share__twitter">
+              <a href="http://twitter.com/intent/tweet?url=<?php the_permalink(); ?>&amp;text=<?php the_title(); ?>&amp;via=unsweeets&amp;related=_X_y_z_" target="_new" id="twitter-share-link">Twitter</a>
+              <script>
+              (function(d, a){
+                var me = d.getElementById(a), href = me.getAttribute('href');
+                me.onclick = function(e){
+                  e.preventDefault();
+                  window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open(href,'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+                };
+              }(document, 'twitter-share-link'));
+              </script>
+            </li>
+            <li class="share__facebook">
+              <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" id="facebook-share-link">Facebook</a>
+              <script>
+              (function(d, a){
+                var me = d.getElementById(a), href = me.getAttribute('href');
+                me.onclick = function(e){
+                  e.preventDefault();
+                  window.twttr=window.twttr||{};var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open(href,'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+                };
+              }(document, 'facebook-share-link'));
+              </script>
+            </li>
             <?php // <li class="share__hatena"><a href="#">Hatena bookmark</a></li> ?>
           </ul>
           <nav>
