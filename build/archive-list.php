4d0dcc3e20aec9d277426bdc83e0d45aa989ccba
sunya <mugen.Xyz@gmail.com>
2015-08-15 21:03:46 +0900

    一覧表示の仕方を修正。

--- build/archive-list.php
+++ build/archive-list.php
@@ -8,15 +8,27 @@ Template Name: archives
   <main id="page__contents" role="main" itemprop="mainEntityOfPage">
     <div id="contents__inner">
       <?php the_content() ?>
-      <section class="single contents__article">
+      <section class="single contents__archive">
         <header class="article__header">
           <h3 class="article__title" itemprop="name"><?php the_title(); ?></h3>
         </header>
         <div class="article__body">
           <?php the_content() ?>
-          <ul id="archive-list">
-          <?php wp_get_archives("type=postbypost"); ?>
-          </ul>
+          <?php $year; ?>
+          <?php query_posts('posts_per_page=-1'); ?>
+          <?php if (have_posts()): while(have_posts()): the_post(); ?>
+          <?php if ($year != get_post_time('Y')): ?>
+            <?php if ($year): ?>
+          </ol>
+            <?php endif; ?>
+            <h4><?php echo get_post_time('Y'); ?>年</h4>
+            <ol class="archive-list">
+            <?php $year = get_post_time('Y'); ?>
+          <?php endif; ?>
+            <li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a> <time datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_post_time('d M Y'); ?></time></li>
+          <?php endwhile; ?>
+          </ol>
+          <?php endif; ?>
         </div>
       </section>
     </div>
--- build/search.php
+++ build/search.php
@@ -8,10 +8,10 @@
           <h1 class="article__title" itemprop="name"><span><?php _e( 'Search Results for:', 'bonestheme' ); ?></span> <?php echo esc_attr(get_search_query()); ?></h1>
         </header>
         <div class="article__body">
-          <ol id="search-results">
+          <ol class="search-results">
               <?php while (have_posts()) : the_post(); ?>
               <li>
-                  <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a> <time datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_post_time('d l Y'); ?></time>
+                  <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a> <time datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_post_time('d M Y'); ?></time>
               </li>
               <?php endwhile; ?>
           </ol>
